8f085fecd72ec2ae609bff46677097f32e9ce9c5
jakobsvolba <jakob.svolba@gmail.com>
2024-10-21 10:38:31 +0200

    add functionality for initialize model and case 1

--- model/main.py
+++ model/main.py
@@ -5,10 +5,10 @@ Created on Tue Oct  14:18:06 2024
 @author: jakobsvolba
 '''
 
+import numpy as np
 
+from model_1_draft import Model_1
 
-
-# from model_1 import model
 
 
 #-------------------------------------------------------------------------------#
@@ -16,10 +16,33 @@ Created on Tue Oct  14:18:06 2024
 # initialize model                                                              #
 #                                                                               #
 #-------------------------------------------------------------------------------#
+model = Model_1(_file_name='model_inputs.xlsx')
 
-# set parameters (for loop for el. Price and Rent)
+#read max_govTariff out of 'model_inputs.xlsx'
 
-# model = model(inputfile, outputadress)
+el_price = np.linspace(0, max_govTariff, 20)
+
+#-------------------------------------------------------------------------------#
+#                                                                               #
+# model run case 1: PV capacity rent                                            #
+#                                                                               #
+#-------------------------------------------------------------------------------#
+pv_rent = np.linspace(1, max_pvRent, 20)
+
+for i in el_price:
+    for j in pv_rent:
+        model.solve(rent=j, elec_price=i)
+        # write output data to a file
+
+
+#-------------------------------------------------------------------------------#
+#                                                                               #
+# model run case 2: feed in tariff                                              #
+#                                                                               #
+#-------------------------------------------------------------------------------#
+
+
+
 
 #-------------------------------------------------------------------------------#
 #                                                                               #
