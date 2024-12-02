a5b2d9f09b9fd8328477b128be80015a39009348
jakobsvolba <jakob.svolba@gmail.com>
2024-12-02 16:24:03 +0100

    fix major bug with feed in tarif

--- model/model_1.py
+++ model/model_1.py
@@ -252,7 +252,7 @@ class Model_1:
                 for d in range(self.days)
                 for h in range(self.hours)
             ) + quicksum(
-                quicksum(feed_in[i, y, d, h] for i in self.house) * self.fit
+                quicksum(feed_in[i, y, d, h] for i in self.house) * self.fit * self.d_weights[d]
                 for d in range(self.days)
                 for h in range(self.hours)
             )
@@ -274,7 +274,7 @@ class Model_1:
                 for d in range(self.days)
                 for h in range(self.hours)
             ) + quicksum(
-                quicksum(feed_in[i, y, d, h] for i in self.house) * self.fit
+                quicksum(feed_in[i, y, d, h] for i in self.house) * self.fit * self.d_weights[d]
                 for d in range(self.days)
                 for h in range(self.hours)
             ))
